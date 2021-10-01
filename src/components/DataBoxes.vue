0167f6ae6557d852db3bd141fc53ede83bd835ea
sahil9001 <sahilsilare@gmail.com>
2021-10-01 12:49:49 +0530

    feat: Added numbers with commas

--- src/components/DataBoxes.vue
+++ src/components/DataBoxes.vue
@@ -8,18 +8,18 @@
             </div>
             <div class="text-2xl mb-4">
                 <span class="font-bold">Total:</span>
-                {{ stats.TotalConfirmed }}
+                {{ numberWithCommas(stats.TotalConfirmed) }}
             </div>
         </div>
         <div class="shadow-md bg-blue-200 p-10 text-center rounded">
             <h3 class="text-3xl text-blue-900 font-bold mb-4">Deaths:</h3>
             <div class="text-2xl mb-4">
                 <span class="font-bold">New:</span>
-                {{ stats.NewDeaths }}
+                {{ numberWithCommas(stats.NewDeaths) }}
             </div>
             <div class="text-2xl mb-4">
                 <span class="font-bold">Total:</span>
-                {{ stats.TotalDeaths }}
+                {{ numberWithCommas(stats.TotalDeaths) }}
             </div>
         </div>
     </div>
