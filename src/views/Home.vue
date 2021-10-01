793e3abca72bf9a5115ff312fa1a7f0e40609877
sahil9001 <sahilsilare@gmail.com>
2021-10-01 10:56:11 +0530

    feat: Readded files

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,44 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main v-if="!loading">
+    <DataTitle :text="title" :dataDate="dataDate" />
+  </main>
+  <main class="flex flex-col align-center justify-center text-center" v-else>
+    <div class="text-grey-300 text-3xl mt-10 mb-6">Loading...</div>
+    <img :src="loadingImage" class="w-24 m-auto" alt="Loading" />
+  </main>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import DataTitle from '@/components/DataTitle'
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    DataTitle
+  },
+  data() {
+    return {
+      loading: true,
+      title: "Global",
+      dataDate: "",
+      stats: {},
+      countries: [],
+      loadingImage: require("../assets/hourglass.gif"),
+    };
+  },
+  methods: {
+    async fetchData() {
+      const response = await fetch("https://api.covid19api.com/summary");
+      const data = await response.json();
+      return data;
+    },
+  },
+  async created() {
+    const data = await this.fetchData();
+    this.dataDate = data.Date
+    this.stats = data.Global
+    this.countries = data.Countries
+    this.loading = false
+  },
+};
 </script>
